7372c5f99a7c25def97b59e31d186e7691171f25
Ming Duan <11203duan@gmail.com>
2019-03-31 20:03:51 +0000

    Automatically backed up by Learn

--- lib/musiclibrarycontroller.rb
+++ lib/musiclibrarycontroller.rb
@@ -57,7 +57,8 @@ end
 def play_song
   puts "Which song number would you like to play?"
   input = gets.chomp
-  if Song.find_by_name(input)
+  if input.class == String
+    Song.find_by_name(input)
     puts "Playing #{input}"
   elsif input <= Song.all.length
     puts "Playing #{@list[input-1].name}"
