52704a5dcbb15704ff032b08711261ab3daf51bf
Ming Duan <11203duan@gmail.com>
2019-03-31 19:49:14 +0000

    Automatically backed up by Learn

--- lib/musiclibrarycontroller.rb
+++ lib/musiclibrarycontroller.rb
@@ -41,8 +41,8 @@ def list_songs_by_artist
   puts "Please enter the name of an artist:"
   input = gets.chomp
   if Artist.find_by_name(input)
-  artist_find = Artist.find_by_name(input)
-  artist_find.songs.sort{|a, b| a.name <=> b.name}.each_with_index{|x, index| puts "#{index+1}. #{x.name} - #{x.genre.name}"}
+  find = Artist.find_by_name(input)
+  find.songs.sort{|a, b| a.name <=> b.name}.each_with_index{|x, index| puts "#{index+1}. #{x.name} - #{x.genre.name}"}
 end
 end
 
@@ -50,8 +50,8 @@ def list_songs_by_genre
   puts "Please enter the name of a genre:"
   input = gets.chomp
   if Genre.find_by_name(input)
-  artist_find = Artist.find_by_name(input)
-  artist_find.songs.sort{|a, b| a.name <=> b.name}.each_with_index{|x, index| puts "#{index+1}. #{x.name} - #{x.genre.name}"}
+  find = Artist.find_by_name(input)
+  find.songs.sort{|a, b| a.name <=> b.name}.each_with_index{|x, index| puts "#{index+1}. #{x.name} - #{x.genre.name}"}
 end
 
   # loop do
