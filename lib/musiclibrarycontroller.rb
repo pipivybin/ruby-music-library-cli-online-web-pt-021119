cb66a4c595a6976936ddae413075c5c085447d8e
Ming Duan <11203duan@gmail.com>
2019-03-31 19:52:10 +0000

    Automatically backed up by Learn

--- lib/musiclibrarycontroller.rb
+++ lib/musiclibrarycontroller.rb
@@ -42,7 +42,7 @@ def list_songs_by_artist
   input = gets.chomp
   # if Artist.find_by_name(input)
   Artist.find_by_name(input).songs.sort{|a, b| a.name <=> b.name}.each_with_index{|x, index| puts "#{index+1}. #{x.name} - #{x.genre.name}"} unless nil
-end
+
 end
 
 def list_songs_by_genre
